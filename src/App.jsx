b15111b27782dfb13d4d4b4277d75416f948d582
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-19 18:54:34 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+      <DaisyNav></DaisyNav>
     </>
   )
 }
